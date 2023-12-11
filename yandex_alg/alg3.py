5c08a1b602699ac09d4bc762035d02106430aa40
sky-sky-code <saidyllapashaev@mail.ru>
2023-12-12 00:17:07 +0300

    алгоритмы яндекс

--- yandex_alg/alg3.py
+++ yandex_alg/alg3.py
@@ -32,3 +32,56 @@ def cubes():
             print(result)
 
 
+import sys
+
+
+def number_words_text():
+    with open(sys.argv[1], 'r') as file:
+        contents = file.read()
+
+    arr = contents.replace('\n', ' ').split(' ')
+    arr.extend([' ', '\n'])
+    print(len(set(arr)))
+
+
+def open_calculator():
+    nums = {i for i in input().split(' ')}
+    number = {i for i in input()}
+    count = len(number - nums)
+    print(count)
+
+
+def alien_genome():
+    genome_one = input().upper()
+    genome_two = input().upper()
+    arr_couple_genome = {f'{genome_one[idx]}{genome_one[idx + 1]}' for idx in range(0, len(genome_one) - 1)}
+    set_couple_genome = {f'{genome_two[idx]}{genome_two[idx + 1]}' for idx in range(0, len(genome_two) - 1)}
+
+    count = 0
+    for couple_genome in arr_couple_genome:
+        if set_couple_genome & {couple_genome}:
+            count += 1
+    print(count)
+
+
+def polyglots():
+    count_polyglots = int(input())
+
+    union_lang = set()
+    inter_lang = set()
+    for i in range(0, count_polyglots):
+        lang = {str(input()) for _ in range(0, int(input()))}
+        if inter_lang == set():
+            inter_lang = lang
+        union_lang = union_lang | lang
+        inter_lang = inter_lang & lang
+
+    print(len(inter_lang))
+    for i in union_lang:
+        print(i)
+    print(len(union_lang))
+    for i in union_lang:
+        print(i)
+
+
+polyglots()

661b6edb0fc0501b5ed2af4557b4df5c0d5834e5
sky-sky-code <saidyllapashaev@mail.ru>
2024-07-21 21:00:34 +0300

    алгоритмы leetcode

--- leetcode/top_interview/medium/array.py
+++ leetcode/top_interview/medium/array.py
@@ -80,3 +80,66 @@ def group_anagrams(strs):
 # Group Anagram
 def group_anagram(arr):
     result = []
+
+
+def jump_game(arr):
+    gas = 0
+
+    for i in arr:
+        if gas < 0:
+            return False
+        elif gas < i:
+            gas = i
+        gas -= 1
+    return True
+
+
+def jump_game_2(nums):
+    ans = end = far = 0
+
+    for i in range(len(nums) - 1):
+        far = max(far, i + nums[i])
+        if far >= len(nums):
+            ans += 1
+            break
+        if i == end:
+            ans += 1
+            end = far
+    return ans
+
+
+def jump_game_v2(nums):
+    near = far = jump = 0
+
+    while far <= len(nums):
+        farthest = 0
+        for i in range(near, far + 1):
+            farthest = max(far, i + nums[i])
+        near = far + 1
+        far = farthest
+        jump += 1
+    return jump
+
+
+def h_index(nums):
+    nums.sort(reverse=True)
+    h = 0
+    for i in range(len(nums)):
+        if nums[i] >= i + 1:
+            h += 1
+        else:
+            break
+    return h
+
+
+def product_except_self(arr):
+    suf = [1] * len(arr)
+    for i in range(len(arr) - 2, -1, -1):
+        suf[i] = suf[i + 1] * arr[i + 1]
+
+    prefix = 1
+    for i in range(1, len(arr)):
+        prefix *= arr[i - 1]
+        suf[i] *= prefix
+    return suf
+

3b00666586ed71cb23d6b43c4ea1be28214f9e9e
sky-sky-code <saidyllapashaev@mail.ru>
2024-08-13 16:45:48 +0300

    композиция Book and Athor

--- gitlearn/git_1.py
+++ gitlearn/git_1.py
@@ -1,2 +1,13 @@
 def hello():
     print('Hello World')
+
+
+class Author:
+    def __init__(self, name):
+        self.name = name
+
+
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = Author(author)

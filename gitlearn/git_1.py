190131608976b31134c2bc2c274a13b28ff84fe1
sky-sky-code <saidyllapashaev@mail.ru>
2024-08-13 16:41:49 +0300

    deleet str

--- gitlearn/git_1.py
+++ gitlearn/git_1.py
@@ -1,20 +1,2 @@
 def hello():
     print('Hello World')
-
-
-def hello_4():
-    print('adslkdjkakjsdjka')
-
-
-def hello_2():
-    print('Hello World')
-
-
-def hello_3():
-    print('Hello World')
-
-
-class Book:
-    def __init__(self, title, author):
-        self.title = title
-        self.author = author
--- gitlearn/new_git.py
+++
@@ -1,6 +0,0 @@
-class Book:
-
-    def __init__(self, title, author, year):
-        self.title = title
-        self.author = author
-        self.year = year
